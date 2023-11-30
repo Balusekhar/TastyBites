0e99c73219c7b82f8c48191191b2852e17b73767
Balu Tirupati <balusekhar8@gmail.com>
2023-11-29 23:33:47 -0800

    Done with Recipe Fragment Design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,12 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    //Navigation Components
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
+    //Shimmer Recycler view
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+
 
 }
